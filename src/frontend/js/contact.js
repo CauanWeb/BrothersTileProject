52c4aa1f15d0056d7f4fafaeb1f9a6dabb687061
idk707 <156857232+idk707@users.noreply.github.com>
2026-03-07 14:34:06 +0000

    Work In Progress

--- src/frontend/js/contact.js
+++ src/frontend/js/contact.js
@@ -17,31 +17,34 @@ document.getElementById('phoneNumber').addEventListener('input', function(e) {
     e.target.value = formatted;
 });
 
-document.getElementById("contactForm").addEventListener("submit", function(e) {
-    e.preventDefault();
-    const firstName = document.getElementById("firstName").value;
-    const lastName = document.getElementById("lastName").value;
-    const phoneNumber = document.getElementById("phoneNumber").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
-    
-    fetch("https://vigilant-potato-r4w7j57qgx93x95j-5000.app.github.dev/contact", {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-        firstName,
-        lastName,
-        phoneNumber,
-        email,
-        message
-    })
-})
-.then(res => res.json())
-.then(data => {
-    alert(data.message);
-    this.reset();
-})
-.catch(err => console.error("Error:", err));
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.getElementById("contactForm").addEventListener("submit", function(e) {
+        e.preventDefault();
+        const firstName = document.getElementById("firstName").value;
+        const lastName = document.getElementById("lastName").value;
+        const phoneNumber = document.getElementById("phoneNumber").value;
+        const email = document.getElementById("email").value;
+        const message = document.getElementById("message").value;
+        
+        fetch("https://obscure-halibut-x5579755gxv5fv4jx-5000.app.github.dev/contact", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            firstName,
+            lastName,
+            phoneNumber,
+            email,
+            message
+        })
+        })
+        .then(res => res.json())
+        .then(data => {
+            alert(data.message);
+            this.reset();
+        })
+        .catch(err => console.error("Error:", err));
+    });
 });
